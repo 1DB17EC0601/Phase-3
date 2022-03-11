1bc86af527dc2f225e85e6d2ff889c067b23c9ca
1DB17EC0601 <98252176+1DB17EC0601@users.noreply.github.com>
2022-03-11 18:16:30 +0530

    phase-3_assisted-practice

---
+++ ApacheKafka/src/main/java/com/ecommerce/ApacheProducerConfig.java
@@ -0,0 +1,18 @@
+package com.ecommerce;
+
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class ApacheProducerConfig {
+	
+	@Bean
+	public ProducerFactory<String , String> producerFactory()
+	{
+		Map<K, V>
+	}
+
+}
